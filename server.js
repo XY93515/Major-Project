00a32b01f7f3b687f04dd868c04d0c2cbe06705a
Paridhi <paridhiguptaaa@gmail.com>
2023-08-02 15:55:52 +0530

    second commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from 'path';
 
 dotenv.config();
 
@@ -21,11 +22,16 @@ app.use(morgan('dev'));
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes);
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome To Ecommerce App</h1>")
-});
+// app.get("/",(req,res)=>{
+//     res.send("<h1>Welcome To Ecommerce App</h1>")
+// });
+
+app.use('*',function(req,res){
+   res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 
 const PORT=process.env.PORT || 8080;
 
